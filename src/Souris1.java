64b9165a3c89eb9e5caa768758572caa861d1e92
Valentin Lamprière <valentin.lampriere1@gmail.com>
2017-03-13 10:21:18 +0100

    Add files via upload

---
+++ src/Souris1.java
@@ -0,0 +1,51 @@
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.tiled.TiledMap;
+
+
+public class Souris1 extends Ennemi{
+
+	private int maxDeplacement;
+	private int compteurPas = 0;
+	
+	public Souris1(TiledMap m, float x, float y)
+	{
+		super(m, x, y);
+		super.var = 1; 
+	}
+	
+	public void init() throws SlickException
+	{
+		moving = true;
+		animations = super.prepareAnimation("BODY_skeleton.png");
+	}
+	
+	public void update(int delta) throws SlickException
+	{
+		float futurX =  getFuturX(delta, 1);
+        float futurY =  getFuturY(delta, 1);
+        maxDeplacement = 500; 
+        
+		
+		boolean collision = isCollision(futurX, futurY);
+		if(collision){
+			setRandomDirection();
+		} else {
+			this.x = futurX;
+			this.y = futurY;
+			compteurPas++;
+			if(compteurPas == maxDeplacement)
+			{
+				setRandomDirection();
+				compteurPas = 0;
+				maxDeplacement = (int) (Math.random()*(500-100)+100);
+			}
+		}
+		
+	}
+}
---
+++ src/Souris2.java
@@ -0,0 +1,53 @@
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Souris2 extends Ennemi {
+
+	double vitesseDeplacement;
+	private Ramzi player;
+	private int tailleHitbox;
+	
+	public Souris2(TiledMap map, Ramzi p, float x, float y) {
+		super(map, x, y);
+		this.player = p;
+		vitesseDeplacement = 0.2;
+		super.var = 2;
+	}
+
+	public void init() throws SlickException {
+		animations = super.prepareAnimation("BODY_skeleton.png");
+	}
+
+	public void update(int delta) throws SlickException
+	{
+		
+		float futurX = getFuturX(delta, 1.5);
+		float futurY = getFuturY(delta, 1.5);
+		float[] vueSouris = super.getVueEnnemi(distanceVue);
+		if (!moving)
+			distanceVue = 100; // La souris va voir le joueur à 100 (px?) ...
+		else {
+			distanceVue = 250; // ... et le perdera de vue à 250 px
+		}
+
+
+		if ((vueSouris[0] < player.getX() && player.getX() < vueSouris[1])
+				&& (vueSouris[2] < player.getY() && player.getY() < vueSouris[3])) // si player dans le rayon de la souris
+		{
+			moving = true;
+		}
+		else
+			moving = false;
+
+		if (moving) {
+
+			if (!super.isCollision(futurX, futurY)) {
+				super.suivrePlayer(player, vitesseDeplacement);
+				// this.x = futurX;
+				// this.y = futurY;
+			} else{
+				super.suivrePlayer(player, 0);
+			}
+		}
+	}
+}
